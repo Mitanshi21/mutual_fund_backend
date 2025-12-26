b636c4812b86c29ecc988f3bb706aaaf70271f50
Mitanshi Lakdawala <mitanshilakdawala21@gmail.com>
2025-12-26 16:57:31 +0530

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using mutual_fund_backend.Models;
+namespace mutual_fund_backend.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<AMC> AMCs { get; set; }
+    }
+}
---
+++ Models/AMC.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mutual_fund_backend.Models
+{
+    [Table("tbl_amc")]
+    public class AMC
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string amcname { get; set; }
+    }
+}
